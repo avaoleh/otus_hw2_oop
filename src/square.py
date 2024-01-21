52d3b99d2690a4e4963baae62dc3a14e977ba6d4
AvaOleh <avakimyanov@gmail.com>
2024-01-21 19:21:29 +0300

    HW2: edited square.py

--- src/square.py
+++ src/square.py
@@ -1,14 +1,25 @@
 """module Class Square"""
-from src.rectangle import Rectangle
+from src.rectangle import Figure
 
 
-class Square(Rectangle):
+class Square(Figure):
     """Class Square"""
 
     def __init__(self, side_a):
-        super().__init__(side_a, side_a)
+        super().__init__(name="Square")
         if side_a <= 0:
-            raise ValueError("нельзя создать квадрат")
+            raise ValueError("нельзя создать прямоугольник")
+        self.__side_a = side_a
+
+    @property
+    def side_a(self):
+        """Getter for attribute __side_a"""
+        return self.__side_a
+
+    @side_a.setter
+    def side_a(self, value):
+        """Setter for attribute __side_a"""
+        self.__side_a = value
 
     def __str__(self):
         """Метод описания (description)"""
